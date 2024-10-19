174c205432cad093b26cf1a342b07018782f8422
r1cardo360 <ric.furlanetto@hotmail.com>
2024-10-19 17:53:28 -0300

    Adicionando Código do Lyniker ao projeto [Insertion_Sort]

---
+++ Insertion_Sort.py
@@ -0,0 +1,23 @@
+from Create_Data import create_data;
+import time
+
+def insertion_sort( lista ):
+  for i in range( 1, len( lista ) ):
+    chave = lista[i]
+    k = i
+    while k > 0 and chave < lista[k - 1]:
+        lista[k] = lista[k - 1]
+        k -= 1
+    lista[k] = chave
+
+
+def Media_Insertion_Sort(tamanho):
+   lista_datas = create_data(tamanho)
+   tempo_inicial = time.time()
+   insertion_sort(lista_datas)
+   tempo_fim = time.time()
+   tempo_final = (tempo_fim - tempo_inicial)
+   return tempo_final
+
+
+   
--- graphics.py
+++ graphics.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 from Heap_Sort import media_heap_sort
 from SelectionSort import media_selection_sort  # Certifique-se de que isso também esteja ajustado
+from Insertion_Sort import Media_Insertion_Sort
 
 # Definindo tamanhos de dados
 quantidades = np.arange(1000, 6000, 1000)
@@ -11,21 +12,24 @@ quantidades = np.arange(1000, 6000, 1000)
 # Dicionário para armazenar os tempos de execução
 tempos_heap_sort = []
 tempos_selection_sort = []
+tempos_insertion_sort = []
 
 for tamanho in quantidades:
     tempos_heap_sort.append(media_heap_sort(tamanho))
-    tempos_selection_sort.append(media_selection_sort(tamanho))  # Assegure-se que essa função também está adaptada
+    tempos_selection_sort.append(media_selection_sort(tamanho))
+    tempos_insertion_sort.append(Media_Insertion_Sort(tamanho))
 
 # Criando um DataFrame
 df = pd.DataFrame({
     'quantidade_dados': quantidades,
     'tempo_heap_sort': tempos_heap_sort,
-    'tempo_selection_sort': tempos_selection_sort
+    'tempo_selection_sort': tempos_selection_sort,
+    'tempo_insertion_sort': tempos_insertion_sort
 })
 
 # Transformando o DataFrame para um formato longo para plotagem
 df_melted = df.melt(id_vars='quantidade_dados', 
-                    value_vars=['tempo_heap_sort', 'tempo_selection_sort'], 
+                    value_vars=['tempo_heap_sort', 'tempo_selection_sort', 'tempo_insertion_sort'], 
                     var_name='algoritmo', 
                     value_name='tempo_algoritmo')
 
